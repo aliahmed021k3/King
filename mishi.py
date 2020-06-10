f517757ad655113dd33ed519e831b7f5ed5ab6dc
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-06-10 15:00:02 +0500

    Update mishi.py

--- mishi.py
+++ mishi.py
@@ -17,7 +17,7 @@ sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30')]
+br.addheaders = [('User-Agent', 'User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 
 
 def keluar():
